d7e7f0b571dcfa68744c8c52de17a412d21930ee
Cvanimschoot <clayton.vanimschoot@gmail.com>
2022-10-29 19:24:22 -0600

    Add new page to allow user to create short URL. Currently just sends to a page that showcases the new ID.

--- express_server.js
+++ express_server.js
@@ -4,6 +4,18 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({ extended: true }));
+
+function generateRandomString() {
+  const valuesForString = '0123456789abcdefghijklmnopqrstuvwxyz';
+  let returnString = '';
+  for (let i = 0; i < 6; i++) {
+    let ranNum = Math.floor((Math.random() * 36));
+    returnString += valuesForString[ranNum];
+  }
+  return returnString;
+}
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -11,13 +23,23 @@ const urlDatabase = {
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
+  res.render("urls_index",  );
 })
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
   res.render("urls_show", templateVars);
 });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send(generateRandomString()); // Respond with 'Ok' (we will replace this)
+});
+
 
 /*
 app.get("/", (req, res) => {
