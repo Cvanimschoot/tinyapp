e6170e1ab577ffd1a45009dfd470bf204545f5c0
Cvanimschoot <clayton.vanimschoot@gmail.com>
2022-10-29 23:04:16 -0600

    Add ability to edit longURL. Create edit button in (/url) for ability to go to edit page

--- express_server.js
+++ express_server.js
@@ -53,6 +53,15 @@ app.post("/urls/:id/delete", (req, res) => {
   res.redirect("/urls/");
 });
 
+app.post("/urls/:id/update", (req, res) => {
+  urlDatabase[req.params.id] = req.body.longURL;
+  res.redirect(`/urls/`);
+});
+
+app.post("/urls/:id/edit", (req, res) =>{
+  res.redirect(`/urls/${req.params.id}`);
+});
+
 
 /*
 app.get("/", (req, res) => {
